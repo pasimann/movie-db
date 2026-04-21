b460ca1fa45b96f144fb69a1e3ec48ecc3e8041b
pasimann <pasimann@gmail.com>
2026-04-21 17:55:40 +0300

    Some implementation for add.

--- src/main/java/com/pasimann/app/model/PersonRepository.java
+++ src/main/java/com/pasimann/app/model/PersonRepository.java
@@ -3,5 +3,6 @@ package com.pasimann.app.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    // public List<Person> getAllPersonsByMovieId(Long id);
+    public Optional<Person> findByFirstNameAndLastNameAndRole(
+        String firstName, String lastName, String role);
 }
--- src/main/java/com/pasimann/app/service/MovieService.java
+++ src/main/java/com/pasimann/app/service/MovieService.java
@@ -31,11 +31,48 @@ public class MovieService {
       return results;
     }
 
-    public MovieItem saveItem(MovieItem item) {
+    public MovieItem saveItem(MovieItem movieItem) {
         // TODO FIX ME: map all the fields 
-        // Persons (actor, director) needs to be persisted as a new person only if not already in the db 
-        Movie movie = new Movie(item.title(), item.description());
-        movieRepository.save(movie);
-        return new MovieItem(movie.getTitle(), movie.getDescription());
+        // Persons (actor, director) could be persisted 
+        // as a new person only if not already in the db 
+        // So we can check the person repo for the given name and role
+        // to check if we need add a new person row or use existing one
+
+        Person movieDirector; 
+
+        Optional<Person> director = personRepository.
+            .findByFirstNameAndLastNameAndRole(
+                movieItem.getDirector().getFirstName(),
+                movieItem.getDirector().getLastName(), 
+                "DIRECTOR"); 
+        
+       if (director.isPresent()) {
+         movieDirector = director.get(); 
+       } else {
+         movieDirector = new Person(movieItem.getDirector().getFirstName(),
+                movieItem.getDirector().getLastName(), 
+                "DIRECTOR"); 
+       }
+
+       List<Person> movieActors = new ArrayList<>(); 
+
+       for (PersonItem item : movieItem.getActors()) {
+         Optional<Person> actor = personRepository.
+            .findByFirstNameAndLastNameAndRole(
+                movieItem.getDirector().getFirstName(),
+                movieItem.getDirector().getLastName(), 
+                "ACTOR"); 
+        
+         if (actor.isPresent()) {
+           movieActors.add(actor.get()); 
+         } else {
+           movieActors.add(new Person(movieItem.getDirector().getFirstName(),
+                 movieItem.getDirector().getLastName(), 
+                 "ACTOR")); 
+         }
+       }
+
+       // TODO add Movie to db with mapped fields; Director and Actors 
+       // might be new ones or existing rows. 
     }
 }
